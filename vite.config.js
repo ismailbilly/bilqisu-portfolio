5f8ff13c64994cf4ff16bb78056a7d5216cc558c
Billy <ismail.bilkisu@gmail.com>
2024-01-20 18:53:03 +0100

    ordered files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/portfolio',
+
   plugins: [react(),
   pluginRewriteAll()
   ],
